6203e50a13c03a51178e83be843e3e557ff2774e
Wisse <wissehes@gmail.com>
2021-08-01 23:47:21 +0200

    Initial Commit

---
+++ Restaurant FInder/Restaurant_FInderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_FInderApp.swift
+//  Restaurant FInder
+//
+//  Created by Wisse Hes on 01/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_FInderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
